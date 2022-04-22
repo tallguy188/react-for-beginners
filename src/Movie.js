68f44ef638cadc4926c96be704139677a34f335c
tallguy188 <cksghd4220@naver.com>
2022-04-22 16:24:15 +0900

    Router-Link

--- src/Movie.js
+++ src/Movie.js
@@ -19,6 +19,8 @@ function Movie() {
 export default Movie;
 
 // movie.js는 이제 router들을 render하는 역할, router는 페이지라고 생각하면 된다.
-// router는 URL을 보고있는 component이다.
+// router는 URL을 보고있는 component이다. Router 안에 들어가는 것은 우리가 유저에게 보여주고 싶은 것이다.
 // switch가 하는 역할은 route를 찾는것, route는 url을 의미하고 route를 찾으면 컴포넌트를 랜더링한다.
 // 우리는 router 안에 route를 만들어줬고, 만약 누군가가 우리 웹사이트의 "/" url에 있다면 우리는 home 컴포넌트를 보여줄거다.
+
+// Link 컴포넌트는 브라우저 새로고침 없이 유저를 다른 페이지로 이동시켜주는 컴포넌트이다.
--- src/components/MyMovie.js
+++ src/components/MyMovie.js
@@ -1,11 +1,14 @@
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 function MyMovie({ coverIMG, title, summary, genres }) {
   //prop의 이름은 그냥 우리 마음대로 지어도 된다.
   return (
     <div>
       <img src={coverIMG} alt={title} />
-      <h2>{title}</h2>
+      <h2>
+        <Link to="/movie">{title}</Link>
+      </h2>
       <p>{summary}</p>
       <ul>
         {genres.map((g) => (
@@ -24,3 +27,5 @@ MyMovie.propTypes = {
 };
 
 export default MyMovie;
+
+// Link 컴포넌트는 브라우저 새로고침 없이 유저를 다른 페이지로 이동시켜주는 컴포넌트이다.
