16a07a1b4e7547b0507776be317be71df3b1ba19
tallguy188 <cksghd4220@naver.com>
2022-05-02 19:37:50 +0900

    style 수정중

--- src/components/MyMovie.js
+++ src/components/MyMovie.js
@@ -5,18 +5,18 @@ import styles from "./MyMovie.module.css";
 function MyMovie({ id, coverImg, title, summary, genres, year }) {
   return (
     <div className={styles.movie}>
-      <img src={coverImg} alt={title} className={styles.movie_image} />
-      <div>
-        <h2 className={styles.movie_title}>
+      <img src={coverImg} alt={title} className={styles.movie__image} />
+      <div className={styles.movie__div}>
+        <h2 className={styles.movie__title}>
           <Link to={`/movie/${id}`}>{title}</Link>
         </h2>
+        <h3 className={styles.movie__year}>{year}</h3>
         <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p>
-        <ul className={styles.movie_genres}>
+        <ul className={styles.movie__genres}>
           {genres.map((g) => (
             <li key={g}>{g}</li>
           ))}
         </ul>
-        <span className={styles.movie_year}>{year}</span>
       </div>
     </div>
   );
